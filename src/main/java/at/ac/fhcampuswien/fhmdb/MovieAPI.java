7a76a664ad67407d14a63a22a283eafe171ed84a
KaMa <maid.kanuric>
2025-03-12 18:56:54 +0100

    Create empty MovieAPI class

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/MovieAPI.java
@@ -0,0 +1,5 @@
+package at.ac.fhcampuswien.fhmdb;
+
+public class MovieAPI {
+    private final String URL = "https://prog2.fh-campuswien.ac.at/swagger-ui/index.html#/movie-controller/getMovies";
+}
